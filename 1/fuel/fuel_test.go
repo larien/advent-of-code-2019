fe517740f79b6da29bb59c9d0519cd570b6e30d8
Lauren Maria Ferreira <lauren.ferremch@gmail.com>
2019-12-20 18:26:51 -0300

    1/ Write test

---
+++ 1/fuel/fuel_test.go
@@ -0,0 +1,27 @@
+package fuel
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCalculator(t *testing.T) {
+	tests := []struct {
+		mass     float64
+		expected int
+	}{
+		{12, 2},
+		// {14, 3},
+		// {1969, 654},
+		// {100756, 33583},
+	}
+	for index, test := range tests {
+		testName := fmt.Sprintf("Calculator (%d)", index)
+		t.Run(testName, func(t *testing.T) {
+			result := Calculator(test.mass)
+			if result != test.expected {
+				t.Errorf("result: %v, expected %v", result, test.expected)
+			}
+		})
+	}
+}
